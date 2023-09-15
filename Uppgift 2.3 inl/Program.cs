96b7dca79913e350f1de51eb9bbb617b9ecf05e5
alve06001 <alve06001@E-PW0234LA>
2023-09-15 13:29:20 +0200

    Add project files.

---
+++ Uppgift 2.3 inl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många kilometer ska du köra?");
+            int km =int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många dagar ska du ha den?");
+            int dagar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Det kommer kosta" + (300 + km + (dagar * 500) - 500) + "kr");
+        }
+
+    }
+
+
+}
